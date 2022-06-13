02024c37dbb192c90ca11fea611354152b46cf48
Nikita Linovytskyi <nekt2111@gmail.com>
2022-06-13 20:35:06 +0300

    test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,5 @@ public class Main {
         ApiContextInitializer.init();
         Bot.runBot(new HelloWorldBot("5303909693:AAGH-wVmTPhhn8mSCvWgezuNGdwRdAXSplM","halushka_pampushka_bot"));
     }
-
+    //test commit
 }
