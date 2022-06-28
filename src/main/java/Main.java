8dd4bed66ede042dce13b25873992fba95ecdf17
Nikita Linovytskyi <nekt2111@gmail.com>
2022-06-28 21:54:54 +0300

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,6 @@ public class Main {
         Bot.runBot(new HelloWorldBot("5303909693:AAGH-wVmTPhhn8mSCvWgezuNGdwRdAXSplM","halushka_pampushka_bot"));
     }
 
-    // TEST COMMIT 21312
+    // TEST COMMIT 21312 3123123123
 
 }
